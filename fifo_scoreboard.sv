cde3356e17a902932a22fb34a29793348ce02adc
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-19 19:11:39 +0530

    Update fifo_scoreboard.sv

--- fifo_scoreboard.sv
+++ fifo_scoreboard.sv
@@ -1,49 +1,36 @@
-class fifo_scoreboard extends uvm_scoreboard;
-  uvm_analysis_imp#(fifo_sequence_item, fifo_scoreboard) item_got_export;
-  `uvm_component_utils(fifo_scoreboard)
-  
-  function new(string name = "fifo_scoreboard", uvm_component parent);
-    super.new(name, parent);
-    item_got_export = new("item_got_export", this);
-  endfunction
-  
-  virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-  endfunction
-  
-  bit [127:0] queue[$];
-   
-  function void write(input fifo_sequence_item item_got);
-    bit [127:0] examdata;
-    if(item_got.i_wren == 'b1)begin
-      if(queue.size() < 1023) begin
-      queue.push_back(item_got.i_wrdata);
-      end
-        else
-          begin
-          $display("---------FIFO IS FULL------------");
-      end
-      `uvm_info("write Data", $sformatf("write: %0d read: %0d wrdata: %0d full: %0d almost full: %0d",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full, item_got.o_alm_full), UVM_LOW);
-    end
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+//`timescale 1ns/10ps
+
+interface fifo_interface(clk,rstn);
+  input clk,rstn;
+    bit o_full,o_empty,o_alm_full,o_alm_empty;
+    bit i_wren,i_rden;
+    bit [127:0] i_wrdata;
+    bit [127:0] o_rddata;
+
+
+clocking d_cb @(posedge clk);
+    default input #1 output #1;
+    output i_wren;
+    output i_rden;
+    output i_wrdata;
+  endclocking
+
+ clocking m_cb @(posedge clk);
+    default input #1 output #1;
+    input i_wren;
+    input i_rden;
+    input i_wrdata;
+    input o_full;
+    input o_empty;
+    input o_alm_full;
+   input o_alm_empty;
+   input o_rddata;
+  endclocking
+
+modport d_mp (input clk, rstn, clocking d_cb);
+  modport m_mp (input clk, rstn, clocking m_cb);
     
-     if (item_got.i_rden == 'b1)begin
-      if(queue.size() >= 'd1)begin
-        examdata = queue.pop_front();
-      end
-       else
-         begin
-           $display("-------FIFO IS EMPTY---------");
-         end
-       `uvm_info("Read Data", $sformatf("examdata: %0d rddata: %0d empty: %0d almost empty: %0d", examdata, item_got.o_rddata, item_got.o_empty, item_got.o_alm_empty), UVM_LOW);
-        if(examdata == item_got.o_rddata)begin
-          $display("-------- Pass! --------");
-        end
-        else begin
-          $display("-------Fail!-------");
-        end
-     end
+endinterface
   
-  endfunction
- 
-endclass
-        
